97eacdaaf9bdcc601ed8150bddafb744e342931a
abishbaig <baig.abish423@gmail.com>
2025-06-04 13:47:21 +0500

    Resolve spacy model loading  issues

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from utils import resume_parser as rp
+
+
 def main():
-    print("Hello from ai-powered-recruitment-agent!")
+    pdf_path = 'docs/resume_1.pdf'
+    parser = rp.ResumeParser()
+    resume_text = parser.extract_text_from_pdf(pdf_path=pdf_path)
+    print(resume_text)
+
 
 
 if __name__ == "__main__":
--- utils/resume_parser.py
+++ utils/resume_parser.py
@@ -1,10 +1,22 @@
 import pdfplumber
+import spacy
+
+
+# Load spaCy model
+try:
+    nlp = spacy.load('en_core_web_sm')
+except:
+    # If the model isn't downloaded, error
+    print("SpaCy model not found. Please download using:")
+    print("uv pip install pip\nuv run --with spacy spacy download en_core_web_sm")
+    # Loading a simple model
+    nlp = spacy.blank('en')
 
 
 class ResumeParser:
+    
 
-
-    def extract_text_from_pdf(pdf_path):
+    def extract_text_from_pdf(self, pdf_path):
         text = ''
         with pdfplumber.open(pdf_path) as pdf:
             for page in pdf.pages:
